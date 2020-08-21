11e8d3bb4fddee502910288e95c4683599db17d0
Shourya Maheshwari <shouryamaheshwari09@gmail.com>
2020-08-21 15:41:55 +0530

    Solution

---
+++ descending_sorted_array.cpp
@@ -0,0 +1,42 @@
+#include<bits/stdc++.h>
+using namespace std;
+int ascendingBinarySearch(int arr[],int n,int key)
+{
+    int start=0;
+    int end=n-1;
+    while(start<=end)
+    {
+        int mid=start+(end-start)/2;
+        if(arr[mid]==key)
+        {
+            return mid;
+        }
+        else if(arr[mid]>key)
+        {
+            start=mid+1;
+        }
+        else
+        {
+            end=mid-1;
+        }
+    }
+    return -1;
+}
+int main()
+{
+    int arr[]={10,9,8,7,6,5,4,3,2,1};
+    int n=sizeof(arr)/sizeof(int);
+    int key;
+    cin>>key;
+    int m=ascendingBinarySearch(arr,n,key);
+    if(m==-1)
+    {
+        cout<<key<<" is not present";
+    }
+    else
+    {
+        cout<<key<<" is present";
+    }
+
+}
+
